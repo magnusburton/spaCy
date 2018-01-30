37776016222bc8d77d313255f2650d38d0f006e6
Magnus Burton <magnus.burton@hotmail.com>
2018-01-30 19:20:01 +0100

    Added more Swedish example sentences

--- spacy/lang/sv/examples.py
+++ spacy/lang/sv/examples.py
@@ -14,5 +14,9 @@ sentences = [
     "Apple överväger att köpa brittisk startup för 1 miljard dollar.",
     "Självkörande bilar förskjuter försäkringsansvar mot tillverkare.",
     "San Fransisco överväger förbud mot leveransrobotar på trottoarer.",
-    "London är en storstad i Storbritannien."
+    "London är en storstad i Storbritannien.",
+    "Var är du?",
+    "Vem är Frankrikes president?",
+    "Vad heter USAs huvudstad?",
+    "När föddes Barack Obama?"
 ]
